349cf02f7c7b3b0556d0efb91e8381aa35387199
s-beats <sakashin10291029@gmail.com>
2022-05-29 20:27:32 +0900

    feat: implement join

--- main.hs
+++ main.hs
@@ -1,9 +1,12 @@
--- main という関数
 main :: IO ()
 main = do
   print (double 2)
+  print (join "a" "b")
 
--- double という関数
 double :: Int -> Int
 double n =
   n * 2
+
+join :: String -> String -> String
+join s1 s2 =
+  s1 ++ s2
