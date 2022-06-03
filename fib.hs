0c626532956118c502d64747574c834d6726fa85
s-beats <sakashin10291029@gmail.com>
2022-06-03 23:21:39 +0900

    feat: fibpnacci case of

--- fib.hs
+++ fib.hs
@@ -9,6 +9,12 @@ fibGuard n
   | n == 1 = 1
   | otherwise = fib (n -1) + fib (n -2)
 
+fibCaseOf n = case n of
+  0 -> 0
+  1 -> 1
+  _ | n > 0 -> fibCaseOf (n -1) + fibCaseOf (n -2)
+
 main = do
   print $ fib 6
   print $ fibGuard 6
+  print $ fibCaseOf 6
