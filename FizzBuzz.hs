aa66d12700b413d0a6bb999c5c70b2a244e1a347
s-beats <sakashin10291029@gmail.com>
2022-06-03 23:12:07 +0900

    feat: fizzbuzz

---
+++ FizzBuzz.hs
@@ -0,0 +1,9 @@
+fizzbuzz n
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 3 == 0 = "Fizz"
+  | n `mod` 5 == 0 = "Buzz"
+  | otherwise = show n
+
+main :: IO ()
+main = do
+  print $ map fizzbuzz [1 .. 50]
