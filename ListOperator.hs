4b9527dc0eb9ed6a557811687e7cbdbc642f5cdb
s-beats <sakashin10291029@gmail.com>
2022-06-05 23:28:15 +0900

    feat: list operator

---
+++ ListOperator.hs
@@ -0,0 +1,7 @@
+main = do
+  print $ length ["hoge", "fuga"]
+  print $ sum [10, 20]
+  print $ product [1, -100]
+  print $ take 3 ["bob", "alice", "mike", "lisa"]
+  print $ drop 3 ["ubuntu", "centos", "archlinux", "selinux"]
+  print $ reverse ["ubuntu", "centos", "archlinux", "selinux"]
