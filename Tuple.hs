7f5fb53afe0f01bea5724fed6ffc482ac98ea314
s-beats <sakashin10291029@gmail.com>
2022-06-07 21:54:24 +0900

    feat: fetch tuple

--- Tuple.hs
+++ Tuple.hs
@@ -7,6 +7,10 @@ main = do
   print c
   print $ fst a
   print $ snd a
+  print t1
+  print t2
   where
     a = divmod 4 2
     (b, c) = divmod 4 2
+    triple = (1, 2, 3)
+    (t1, t2, _) = triple
