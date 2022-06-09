2d8baaaf989bed1952f7cff0df5ba9ed943645db
s-beats <sakashin10291029@gmail.com>
2022-06-10 08:43:39 +0900

    feat: bubble sort

--- BubbleSort.hs
+++ BubbleSort.hs
@@ -3,5 +3,11 @@ swap (x : y : ys)
   | x < y = y : swap (x : ys)
   | otherwise = x : swap (y : ys)
 
+bubbleSort [] = []
+bubbleSort xs = x : bubbleSort ys
+  where
+    (x : ys) = reverse $ swap xs
+
 main = do
-  print $ swap [3, 5, 1, 2]
+  print $ swap [0, 3, 5, 4, 1, 2]
+  print $ bubbleSort [0, 3, 5, 4, 1, 2]
