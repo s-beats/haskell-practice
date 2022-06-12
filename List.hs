aa023841fa28ea41f0d416f2cd85e48dd8142a80
s-beats <sakashin10291029@gmail.com>
2022-06-12 10:06:56 +0900

    feat: list

--- List.hs
+++ List.hs
@@ -1,9 +1,14 @@
 import Control.Monad (when)
 
+addOne 0 = 1
+addOne n | n > 0 = n + 0x1
+
 main = do
   let listInt = [1, 2, 3, 4, 5]
   print listInt
   print listString
   print $ listInt !! 4
+  print [addOne x | x <- [1, 2, 3, 4, 5]]
+  print [addOne x | x <- [1, 2, 3, 4, 5], x `mod` 2 == 0]
   where
     listString = ["hoge", "huga", "piyo"]
