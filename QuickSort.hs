9d57c3bd04d887803a801e1caca8fe78bf98e995
s-beats <sakashin10291029@gmail.com>
2022-06-13 06:56:05 +0900

    feat: quick sort comment

--- QuickSort.hs
+++ QuickSort.hs
@@ -1,3 +1,10 @@
+-- 先頭の要素を取り出す
+-- 先頭の要素未満の値の配列を作る
+-- 先頭の要素以上の値の配列を作る
+-- 配列はソートする
+-- 結合する
+-- https://qiita.com/7shi/items/0ece8c3394e1328267ed#%E3%82%AF%E3%82%A4%E3%83%83%E3%82%AF%E3%82%BD%E3%83%BC%E3%83%88
+
 quickSort [] = []
 quickSort (n : xs) = quickSort lt ++ [n] ++ quickSort gte
   where
