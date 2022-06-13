02c173cc92c083e1430e8dc6f1f600b68e7ae937
s-beats <sakashin10291029@gmail.com>
2022-06-13 23:07:02 +0900

    fest: pair

--- List.hs
+++ List.hs
@@ -10,5 +10,6 @@ main = do
   print $ listInt !! 4
   print [addOne x | x <- [1, 2, 3, 4, 5]]
   print [addOne x | x <- [1, 2, 3, 4, 5], x `mod` 2 == 0]
+  print [(x, y) | x <- [1, 2, 3], y <- [3, 2, 1]]
   where
     listString = ["hoge", "huga", "piyo"]
