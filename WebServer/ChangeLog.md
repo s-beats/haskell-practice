785c301e5a866d7b6c9bfc67d91de7bfcf0715a4
s-beats <sakashin10291029@gmail.com>
2022-06-15 08:49:58 +0900

    feat: web server

---
+++ WebServer/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for WebServer
+
+## Unreleased changes
---
+++ WebServer/app/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Network.HTTP.Types as HTypes
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+
+main :: IO ()
+main = Warp.run 3000 pingHandler
+
+router :: Wai.Application
+router req send = case Wai.pathInfo req of
+  ["ping"] -> pingHandler req send
+  _ -> notFoundHandler req send
+
+pingHandler :: Wai.Application
+pingHandler req send = send $ Wai.responseBuilder HTypes.status200 [] "pong"
+
+notFoundHandler :: Wai.Application
+notFoundHandler req send = send $ Wai.responseBuilder HTypes.status404 [] "not found"
