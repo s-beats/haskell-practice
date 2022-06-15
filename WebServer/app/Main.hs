fb6a584d28b304c7aa1ff5e7855234b0fb478b3a
s-beats <sakashin10291029@gmail.com>
2022-06-15 19:35:29 +0900

    feat: ping pong

--- WebServer/app/Main.hs
+++ WebServer/app/Main.hs
@@ -7,7 +7,7 @@ import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 
 main :: IO ()
-main = Warp.run 3000 pingHandler
+main = Warp.run 3000 router
 
 router :: Wai.Application
 router req send = case Wai.pathInfo req of
