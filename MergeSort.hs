af0efef846db32e9039650b7f8f8b2af96a483e3
s-beats <sakashin10291029@gmail.com>
2022-06-18 08:00:42 +0900

    feat: debug merge sort

--- MergeSort.hs
+++ MergeSort.hs
@@ -1,12 +1,14 @@
-merge xs [] = xs
-merge [] ys = ys
-merge (x : xs) (y : ys)
-  | x < y = x : merge xs (y : ys)
-  | otherwise = y : merge (x : xs) ys
+import Debug.Trace
 
-mergeSort [] = []
-mergeSort [x] = [x]
-mergeSort xs = merge (mergeSort (take h xs)) (mergeSort (drop h xs))
+merge xs [] = trace ("A " ++ show xs ++ []) xs
+merge [] ys = trace ("B " ++ show ys ++ []) ys
+merge (x : xs) (y : ys)
+  | x < y = trace ("C " ++ show (x : xs) ++ show (y : ys)) x : merge xs (y : ys)
+  | otherwise = trace ("D " ++ show (x : xs) ++ show (y : ys)) y : merge (x : xs) ys
+
+mergeSort [] = trace ("E ") []
+mergeSort [x] = trace ("F " ++ show [x]) [x]
+mergeSort xs = trace ("G " ++ show xs) merge (mergeSort (take h xs)) (mergeSort (drop h xs))
   where
     h = (length xs) `div` 2
 
