953110b03820af4f00032d31a664213a6d195f6f
s-beats <sakashin10291029@gmail.com>
2022-06-20 08:58:35 +0900

    feat: enum

---
+++ Enum.hs
@@ -0,0 +1,11 @@
+data OS = Mac | Windows | Linux | IOS deriving (Show, Enum)
+
+main = do
+  print $ fromEnum Mac
+  print $ fromEnum Windows
+  print $ fromEnum Linux
+  print $ fromEnum IOS
+  print (toEnum 0 :: OS)
+  print (toEnum 1 :: OS)
+  print (toEnum 2 :: OS)
+  print (toEnum 3 :: OS)
