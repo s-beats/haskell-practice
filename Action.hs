13a040cf3633cc36e0b3eb83931e6434c7dc7f1c
s-beats <sakashin10291029@gmail.com>
2022-06-21 21:15:51 +0900

    feat: action

--- Action.hs
+++ Action.hs
@@ -1,8 +1,17 @@
-import System.Random
+onlyA :: Monad m => Char -> m [Char]
+onlyA 'a' = return "ok"
+onlyA _ = return "ng"
 
-randAlpha = randomRIO ('a', 'd')
+fact :: (Eq a, Num a, Monad m) => a -> m a
+fact 0 = return 1
+fact n = do
+  n' <- fact (n - 1)
+  return $ n * n'
+
+-- fact n | n > 0 = n * fact (n - 1)
 
 main = do
-  r <- randAlpha
+  r <- onlyA 'a'
   print r
-  if r == 'd' then print "end" else main
+  if r == "ok" then print "end" else main
+  print =<< fact 5
