6195eb5cd175ed30f2b38a7a290a60a6cb51b5e9
s-beats <sakashin10291029@gmail.com>
2022-06-22 19:53:51 +0900

    feat: sub one

--- List.hs
+++ List.hs
@@ -1,7 +1,14 @@
 import Control.Monad (when)
 
+addOne :: (Num p, Ord p) => p -> p
 addOne 0 = 1
 addOne n | n > 0 = n + 0x1
+
+subOne :: (Num p, Ord p) => p -> p
+subOne 0 = 0
+subOne n | n > 0 = n - 0x1
+subOne n | n < 0 = n + 0x1
+subOne n | n == 0 = 0x0
 
 main = do
   let listInt = [1, 2, 3, 4, 5]
@@ -11,5 +18,7 @@ main = do
   print [addOne x | x <- [1, 2, 3, 4, 5]]
   print [addOne x | x <- [1, 2, 3, 4, 5], x `mod` 2 == 0]
   print [(x, y) | x <- [1, 2, 3], y <- [3, 2, 1], x - y == 0]
+  print [subOne x | x <- [1, 2, 3, 4, 5]]
+  print [subOne x | x <- [1, 2, 3, 4, 5], x `mod` 2 == 0]
   where
     listString = ["hoge", "huga", "piyo"]
